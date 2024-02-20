f7dcf5e198c18c4dc2255d302814a079b18439e4
roshan <rosanstha2059X@gmail.com>
2024-02-20 19:57:44 +0545

    listview

---
+++ lib/listviewassignment.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_2/listview.dart';
+
+class CustomGridView2 extends StatelessWidget {
+  const CustomGridView2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CustomGridView(),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-
-import 'listview.dart';
+import 'package:flutter_application_2/listviewassignment.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(body: CustomGridView()),
+      home: Scaffold(body: CustomGridView2()),
     );
   }
 }
