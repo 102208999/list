5d5df0d2b80bdc6aa16512505e2c6944ca0c03eb
roshan <rosanstha2059X@gmail.com>
2024-02-20 15:32:16 +0545

    task

--- lib/listview.dart
+++ lib/listview.dart
@@ -1,10 +1,48 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
-class ListView extends StatelessWidget {
-  const ListView({super.key});
+class CustomGridView extends StatefulWidget {
+  const CustomGridView({super.key});
+
+  @override
+  State<CustomGridView> createState() => CustomGridViewState();
+}
+
+class CustomGridViewState extends State<CustomGridView> {
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      child: GridView.count(
+        crossAxisCount: 4,
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+        children: [
+          gridItem(),
+          gridItem(),
+          gridItem(),
+        ],
+      ),
+    );
+  }
+}
+
+class gridItem extends StatelessWidget {
+  const gridItem({
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+        height: 100,
+        width: 100,
+        decoration: BoxDecoration(),
+        child: Image(
+          image: AssetImage(
+            "1_Nature_1.png",
+          ),
+          fit: BoxFit.cover,
+        ));
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
-// ignore_for_file: prefer_const_constructors
-
 import 'package:flutter/material.dart';
-import 'package:flutter_application_2/incomplete.dart';
+
+import 'listview.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(body: Incomplete()),
+      home: Scaffold(body: CustomGridView()),
     );
   }
 }
