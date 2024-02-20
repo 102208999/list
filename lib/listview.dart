237d2e2c6df9f53e24692507a5e0df28c564de4d
roshan <rosanstha2059X@gmail.com>
2024-02-20 13:38:11 +0545

    listview file created

---
+++ lib/listview.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ListView extends StatelessWidget {
+  const ListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
